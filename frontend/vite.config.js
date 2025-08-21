2aaf838814c3375431cbb858be06faa606f3e0b8
snowleaopardxt <gunjit15@gmail.com>
2025-08-21 21:38:24 +0530

    netlify configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // important for relative paths
+  base: "./",   // 🔑 ensures relative paths
+  build: {
+    outDir: "dist", // Netlify expects dist
+  },
 });
